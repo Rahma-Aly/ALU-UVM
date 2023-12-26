5d73657e080f4af325b47fc083ae1c28ff2f180e
Rahma Aly <71734575+Rahma-Aly@users.noreply.github.com>
2023-12-26 22:19:54 +0200

    Update ALU_Sequence.sv

--- ALU_Sequence.sv
+++ ALU_Sequence.sv
@@ -11,7 +11,7 @@ class ALU_Sequence extends uvm_sequence #(ALU_Transaction);
     ALU_Transaction    alu_trans;
     int unsigned       n     = 3000;
 
-    function new (string name = "alu_seq");  //no default name each sequence has its unique name
+    function new (string name = "alu_seq"); 
       super.new(name);
     endfunction
     
